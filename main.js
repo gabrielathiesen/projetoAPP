e17dc17cefc763c24f05e2cc41067efb53933bad
unknown <gabriela_thiesen@saojose.sc.senai.br>
2019-07-03 08:57:42 -0300

    arrumei a ligação entre index e js

--- main.js
+++ main.js
@@ -2,3 +2,29 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
+var todos = [
+    "fazer cafe",
+    "estudar java",
+    "acessar comunidade"
+];
+
+function renderTodos(){
+    for(todo of todos){
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+
+function addTodo(){
+    var todoText = inputElement.nodeValue;
+
+    todo.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+}
