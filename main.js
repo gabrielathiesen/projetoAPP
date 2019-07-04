9f3aa2fa986468e66d3eef0ce0d292ac3223997d
giuliasbors <48677328+giuliasbors@users.noreply.github.com>
2019-07-04 11:32:19 -0300

    Arrumando erros

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ var todos = [
 ];
 
 function renderTodos(){
+listElement.innerHTML = '';
+
     for(todo of todos){
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
@@ -22,9 +24,10 @@ renderTodos();
 
 
 function addTodo(){
-    var todoText = inputElement.nodeValue;
+    var todoText = inputElement.value;
 
-    todo.push(todoText);
+    todos.push(todoText);
     inputElement.value = '';
     renderTodos();
 }
+buttonElement.onclick = addTodo;
