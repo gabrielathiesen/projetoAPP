997fc64ebe5fb604212a5b227b780c111418d3cf
sophiasalvan <51633057+sophiasalvan@users.noreply.github.com>
2019-07-04 21:20:15 -0300

    Adicionei botões das categorias

--- main.js
+++ main.js
@@ -2,51 +2,53 @@ var rowElement = document.querySelector('#app tr');
 console.log(rowElement);
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
-
+/*
 var linhas = [
     { nome:"Giulia", cor:"amarelo"},
     { nome:"Sophia", cor:"rosa"},
     { nome:"Gabriela", cor:"preta"}
 ];
+*/
 
-/*
 function renderTodos(){
 rowElement.innerHTML = '';
 
     for (linha of linhas){
-        var linhaElement = document.createElement('');
-        var linhaText = document.createTextNode(linha);
+        var linhasElement = document.createElement('th');
+        var linhasText = document.createTextNode(linha);
 
-        var linkElement = document.createElement('a')
+        var linkElement = document.createElement('a');
 
         linkElement.setAttribute('href' , '#');
 
-        var linkText = document.createTextNode (' Excluir')
+        var linkText = document.createTextNode (' Excluir');
 
         linkElement.appendChild(linkText);
 
-         var pos = todos.indexOf(todo);
+         var pos = linhas.indexOf(linha);
 
-         linkElement.setAttribute('onclick', 'deleteTodo('+ pos +')' );
+         linkElement.setAttribute('onclick', 'deleteTodo'(+ pos +''));
 
-        linhaElement.appendChild(linhaText);
-        linhaElement.appendChild(linkElement);
+        linhasElement.appendChild (linhasText);
+        linhasElement.appendChild(linkElement);
 
-        rowElement.appendChild(linhaElement);
+        rowElement.appendChild(linhasElement);
     }
 }
 
 renderLinhas();
 
 
-function addTodo(){
+function addLinha(){
     var linhaText = inputElement.value;
 
     linhas.push(linhaText);
     inputElement.value = '';
     renderLinhas();
 }
+
 buttonElement.onclick = addLinha;
+/*
 function deleteLinha(pos) {
     linhas.splice(pos, 1);
     renderLinhas();
